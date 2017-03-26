8c6dde334b52ab733c39735fa78d537ec72de2aa
HP-450G2\Admin <lameklena@gmail.com>
2017-03-26 18:53:00 +0200

    f

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// 8.7 tablice 
 var femaleNames = ['Ola', 'Kasia', 'Asia', 'Justyna'];
 var maleNames = ['Kuba', 'Sebastian', 'Krzysiu', 'Jacek'];
 var allNames = femaleNames.concat(maleNames);
@@ -10,3 +11,17 @@ if (allNames.indexOf(newName) === -1) {
 }
 
 console.log( allNames );
+
+// 8.9 choinka 
+function rysujChoinke(n) {
+	for ( var k = 0 ; k < n; k++) {
+		var star = ' ';
+			for ( var j = 0 ; j < k*2-1; j++) {
+				var star = star + '*';
+			}
+			console.log(star);
+	}
+	
+}
+
+rysujChoinke(6);
